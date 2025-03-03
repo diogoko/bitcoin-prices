fd3554c5a4023d1f1133c7d19065f8560c4822c4
Diogo Kollross <diogoko@gmail.com>
2025-03-03 10:24:28 -0300

    Add basic backend project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+
+const resultNumberFormat = Intl.NumberFormat('en', {
+  maximumFractionDigits: 5
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fromValue, setFromValue] = useState('');
+  const livePrice = 62445.0;
+
+  const fromValueNumber = parseFloat(fromValue);
+  const toValue = isNaN(fromValueNumber) ? null : fromValueNumber / livePrice;
 
   return (
-    <>
-      <div className="text-2xl text-green-800 font-bold">hello</div>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <div className="border p-4 flex flex-col gap-4">
+        <h3>Live Price ($): {livePrice}</h3>
+
+        <input
+          type="number"
+          value={fromValue}
+          onChange={(event) => setFromValue(event.target.value)}
+          className="p-1 border rounded-sm"
+        />
+
+        {toValue !== null && (
+          <div>{resultNumberFormat.format(toValue)} Bitcoin</div>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
