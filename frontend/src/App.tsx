cd083dc41c43e2ca1fc0e46d6476cca66593e2fa
Diogo Kollross <diogoko@gmail.com>
2025-03-02 23:01:33 -0300

    Configure Tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+      <div className="text-2xl text-green-800 font-bold">hello</div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
